29be0a71f1c395eb5dff44bc3a62eb6bb4b752c5
camora1990 <moorales.camilo@gmail.com>
2022-07-17 11:31:07 -0500

    [Refactoring] se implementa el llamado al menu del juego

--- src/main/java/org/sofka/Main.java
+++ src/main/java/org/sofka/Main.java
@@ -1,15 +1,14 @@
 package org.sofka;
 
-import org.sofka.bootstrap.DataBaseConnection;
-import org.sofka.dao.CategoryDao;
+
+import org.sofka.ui.ViewGame;
 
 
 public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println(  DataBaseConnection.getDatabase().getName());
-        CategoryDao categoryDao = new CategoryDao();
-        System.out.println(categoryDao.getCategoriesByLevel(1).first().toJson());
+        ViewGame.menuGame();
     }
+
 }
