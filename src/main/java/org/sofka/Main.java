90847863c22f4c1ba86571325a74cf99141403da
camora1990 <moorales.camilo@gmail.com>
2022-07-16 19:20:55 -0500

    se crea el punto de entrada del app

--- src/main/java/org/sofka/Main.java
+++ src/main/java/org/sofka/Main.java
@@ -1,7 +1,9 @@
 package org.sofka;
 
+import org.sofka.bootstrap.DataBaseConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
